8c94e45bb89962d603d52852bbc64e425862705f
Pedro Pinto <antoniopedrosilvapinto+github@gmail.com>
2020-10-17 11:33:28 +0100

    Added size prop.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { Col, Form } from "react-bootstrap";
-import "./styles.scss";
 import ProgressSpinner from "./ProgressSpinner";
+import "./styles.scss";
 
 export default function App() {
   const [progress, setProgress] = useState(70);
   const [strokeWidth, setStrokeWidth] = useState(6);
-  const [spin, setSpin] = useState(false);
+  const [spin, setSpin] = useState(true);
   const [variant, setVariant] = useState("primary");
+  const [size, setSize] = useState("lg");
 
   const max = 100;
 
@@ -17,14 +18,17 @@ export default function App() {
       <p />
 
       <ProgressSpinner
-        size={60}
+        size={size}
         strokeWidth={strokeWidth}
-        max={max}
+        maxProgress={max}
         progress={progress}
         spin={spin}
         variant={variant}
       >
-        <div className="spinner-progress">{progress}%</div>
+        <div className="spinner-progress">
+          {Math.floor(progress)}
+          <span style={{ fontSize: "0.7rem" }}> %</span>
+        </div>
       </ProgressSpinner>
       <p />
 
@@ -36,7 +40,7 @@ export default function App() {
               size="sm"
               type="number"
               defaultValue={progress}
-              step={1}
+              step={0.25}
               min={0}
               max={max}
               onChange={({ target: { value } }) => setProgress(Number(value))}
@@ -68,11 +72,28 @@ export default function App() {
               defaultValue={strokeWidth}
               step={1}
               min={1}
-              max={30}
+              max={
+                // assumes 1rem === 16px
+                size === "sm" ? 8 : size === "md" ? 16 : size === "lg" ? 24 : 32 // assume 'xl'
+              }
               onChange={({ target: { value } }) =>
                 setStrokeWidth(Number(value))
               }
             />
+          </Form.Group>
+          <Form.Group as={Col} sm="3" controlId="size">
+            <Form.Label>Size</Form.Label>
+            <Form.Control
+              size="sm"
+              as="select"
+              defaultValue={size}
+              onChange={({ target: { value } }) => setSize(value)}
+            >
+              <option value="sm">Small</option>
+              <option value="md">Medium</option>
+              <option value="lg">Large</option>
+              <option value="xl">Extra Large</option>
+            </Form.Control>
           </Form.Group>
         </Form.Row>
         <Form.Row>
--- src/ProgressSpinner.tsx
+++ src/ProgressSpinner.tsx
@@ -1,13 +1,13 @@
+import React from "react";
 import classNames from "classnames";
-import React, { useEffect, useRef, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./progress-spinner.scss";
 
 interface Props {
-  size: number;
   progress: number;
-  min?: number;
-  max?: number;
+  minProgress?: number;
+  maxProgress?: number;
+  size?: "sm" | "md" | "lg" | "xl" | string;
   strokeWidth?: number;
   variant?: string;
   className?: string;
@@ -15,48 +15,156 @@ interface Props {
   children?: any;
 }
 
-export default function ProgressSpinner({
-  size,
-  progress = 0,
-  min = 0,
-  max = 1,
-  strokeWidth = 1,
-  variant = "primary",
-  className = "",
-  spin = false,
-  children
-}: Props) {
-  const center = size / 2;
+interface State {
+  size: number;
+  strokeWidth: number;
+}
 
-  progress = progress < min ? min : progress > max ? max : progress;
-  progress = (progress - min) / (max - min);
+export default class ProgressSpinner extends React.Component<Props, State> {
+  static defaultProps: Partial<Props> = {
+    progress: 0,
+    minProgress: 0,
+    maxProgress: 1,
+    strokeWidth: 1,
+    size: "md",
+    variant: "primary",
+    className: "",
+    spin: false
+  };
 
-  strokeWidth =
-    strokeWidth < 1 ? 1 : strokeWidth > center ? center : strokeWidth;
+  private ref: any;
+  private radius: number;
+  private circumference: number;
+  private observer: MutationObserver;
 
-  const radius = center - strokeWidth / 2;
-  const circumference = radius * 2 * Math.PI;
-  const offset = (1 - progress) * circumference;
+  state: State = {
+    size: 0,
+    strokeWidth: 0
+  };
 
-  return (
-    <div className={classNames("progress-spinner", className)}>
-      <svg viewBox={`0 0 ${size} ${size}`} width={size}>
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+  }
+
+  componentDidMount() {
+    const { width } = this.ref.current.getBoundingClientRect();
+    this.setSize(width);
+
+    this.observer = new MutationObserver(this.observerCallback);
+    this.observer.observe(this.ref.current, { attributes: true });
+  }
+
+  observerCallback = () => {
+    this.setState((state) => {
+      const { width: newSize } = this.ref.current.getBoundingClientRect();
+      if (newSize !== state.size) {
+        const {
+          strokeWidth,
+          radius,
+          circumference
+        } = this.recalculateCircleParameters(newSize);
+        this.radius = radius;
+        this.circumference = circumference;
+        return { size: newSize, strokeWidth };
+      }
+      return {};
+    });
+  };
+
+  componentWillUnmount() {
+    if (this.observer) {
+      this.observer.disconnect();
+      delete this.observer;
+    }
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.strokeWidth !== prevProps.strokeWidth && this.props.size) {
+      this.updateStrokeWidth(this.state.size);
+    }
+  }
+
+  setSize(size: number): void {
+    this.setState({ size });
+
+    if (!size) {
+      return;
+    }
+
+    this.updateStrokeWidth(size);
+  }
+
+  updateStrokeWidth(size: number) {
+    const {
+      strokeWidth,
+      radius,
+      circumference
+    } = this.recalculateCircleParameters(size);
+
+    this.setState({ strokeWidth });
+    this.radius = radius;
+    this.circumference = circumference;
+  }
+
+  recalculateCircleParameters(size: number) {
+    const strokeWidth = clamp(this.props.strokeWidth, 1, size / 2);
+    const radius = (size - strokeWidth) / 2;
+    const circumference = radius * 2 * Math.PI;
+
+    return { strokeWidth, radius, circumference };
+  }
+
+  render() {
+    const { className, size, children } = this.props;
+
+    return (
+      <div
+        className={classNames(
+          className,
+          "progress-spinner",
+          `progress-spinner-${size}`
+        )}
+        ref={this.ref}
+      >
+        {this.renderSpinner()}
+        {children}
+      </div>
+    );
+  }
+
+  renderSpinner() {
+    const { size } = this.state;
+
+    if (!size) {
+      return null;
+    }
+
+    const { variant, spin, minProgress, maxProgress } = this.props;
+
+    let progress = clamp(this.props.progress, minProgress, maxProgress);
+    progress = (progress - minProgress) / (maxProgress - minProgress);
+
+    const offset = (1 - progress) * this.circumference;
+
+    return (
+      <svg viewBox={`0 0 ${size} ${size}`}>
         <circle
           className={classNames("spinner-circle", `text-${variant}`, {
             spin,
-            paused: progress === 0 || progress === 1
+            paused: [0, 1].includes(progress)
           })}
-          fill="none"
-          stroke="currentColor"
-          strokeWidth={strokeWidth}
           cx="50%"
           cy="50%"
-          r={radius}
+          r={this.radius}
+          strokeDasharray={`${this.circumference} ${this.circumference}`}
+          strokeWidth={this.state.strokeWidth}
           strokeDashoffset={offset}
-          strokeDasharray={`${circumference} ${circumference}`}
         ></circle>
       </svg>
-      {children}
-    </div>
-  );
+    );
+  }
 }
+
+const clamp = (value: number, min: number, max: number) =>
+  Math.min(Math.max(value, min), max);
